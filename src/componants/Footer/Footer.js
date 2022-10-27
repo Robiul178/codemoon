477d76dcea27e7845f0478f83b69ca748b46889b
Robiul Islam <robiulislam.me2022@gmail.com>
2022-10-27 20:45:32 +0600

    changing icon

--- src/componants/Footer/Footer.js
+++ src/componants/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className='footer mt-10'>
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
                 <div>
-                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2022 - All right reserved by Codecademy </p>
                 </div>
             </footer>
         </div>
