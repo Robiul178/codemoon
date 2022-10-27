eb9d398d87eb63f1953da773e85615c2ce20f4b8
Robiul Islam <robiulislam.me2022@gmail.com>
2022-10-27 21:34:26 +0600

    404 page

--- src/componants/FAQ.js
+++ src/componants/FAQ.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CSS/Faq.css'
 
 const FAQ = () => {
     return (
         <div>
-            <div>
+            <div className='faq-page'>
                 <div className='relative top-24 text-lg'>
                     <h1>Sorry</h1>
                     <h3>Not complite this page</h3>
--- src/componants/NotFound404/NotFound404.js
+++ src/componants/NotFound404/NotFound404.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../CSS/NotFound404.css'
 
 const NotFound404 = () => {
     return (
-        <div>
-            <div className='absolute top-20 left-20 text-lg'>
+        <div className=''>
+            <div className='not-found text-lg'>
                 <h1>Sorry</h1>
                 <h3>You search wrong page.</h3>
                 <Link to='/'>Click here for Back to home page</Link>
