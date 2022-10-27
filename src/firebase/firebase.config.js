bb75f97f79582fe9f93cc20464c2c3722603c491
Robiul Islam <robiulislam.me2022@gmail.com>
2022-10-27 21:14:03 +0600

    added  Environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA4r33tw2zBgxQYQAE20CFka4UH0zqB3hA",
-    authDomain: "codecademy-3b964.firebaseapp.com",
-    projectId: "codecademy-3b964",
-    storageBucket: "codecademy-3b964.appspot.com",
-    messagingSenderId: "116793080631",
-    appId: "1:116793080631:web:11169aa02f5baf1d5ab8b2",
-    measurementId: "G-GW4ML1PE1H"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
